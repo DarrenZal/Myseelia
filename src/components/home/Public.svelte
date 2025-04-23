c017abd43f6d44baf919de23c303f204ffe27bcd
DarrenZal <zaldarren@gmail.com>
2025-04-22 18:54:25 -0700

    hide try refichat text

--- src/components/home/Public.svelte
+++ src/components/home/Public.svelte
@@ -6,7 +6,7 @@
   class="min-h-[calc(100vh-96px)] flex flex-col items-start justify-center max-w-[700px] m-auto gap-6 pb-5 text-sm"
 >
   <h1 class="text-xl">Welcome to {appName}</h1>
-  <a class="btn btn-primary" href="/Chat">Try ReFi Chat</a>
+  <!-- Removed Try ReFi Chat button -->
   <div class="max-w-[590px]">
     <p class="mb-5">
       This app uses passwordless login based on keys generated in your browser. Click Register to create a new identity and encrypted filesystem.
